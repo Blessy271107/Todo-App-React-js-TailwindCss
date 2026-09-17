3c766712b490ef63a9adbf2c48b46f1ad796c662
Blessy271107 <reetashanthi92@gmail.com>
2026-09-17 16:15:48 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  base:'/Todo-App-Using-React-js-TailwindCss.git/'
+  base:'/Todo-App-React-js-TailwindCss/'
 })
 
